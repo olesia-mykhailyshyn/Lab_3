328b98ef94bd66c0c776cb14d8ad7a4e159240d8
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2024-07-09 17:08:26 +0300

    update task 1

--- Lab_3/task_1.py
+++ Lab_3/task_1.py
@@ -1,43 +1,34 @@
 # A=Um*m(otrhogonal- rotate) * Em*n(diagonal - stretch) V_transponedn*n(otrhogonal - rotate)
 import numpy as np
 
+matrix = np.array([[2, 8],
+                  [7, 1],
+                  [6, 9]])
 
-#matrix = np.array([[5, 3, 2], [9, 7, 2]])
-matrix = np.array([[2, 8, 7, 9, 1],
-                  [7, 1, 4, 7, 7],
-                  [6, 9, 9, 2, 2]])
-
-
-def is_orthonormal(vectors):
-    #якщо вектори ортонормальні, то їх добуток = одинична матриця
-    return np.allclose(np.dot(vectors.T, vectors), np.eye(vectors.shape[0])) #eye створює одиничну матрицю
-
-
-def normalize_vectors(vectors):
-    for i in range(vectors.shape[1]):
-        vectors[:, i] /= np.linalg.norm(vectors[:, i])
-    return vectors
+# def generate_random_matrix(max_size=5):
+#     rows = np.random.randint(1, max_size + 1)
+#     cols = np.random.randint(1, max_size + 1)
+#     return np.random.randint(0, 10, size=(rows, cols))
 
 
 def svd(matrix):
     Sr = np.dot(matrix, matrix.T)
 
     eigenvalues, eigenvectors = np.linalg.eig(Sr)
-    sorted = np.argsort(eigenvalues)[::-1] # [:: -1] -- зміна порядку індексів, щоб було спадання
+    sorted = np.argsort(eigenvalues)[::-1]
     eigenvalues = eigenvalues[sorted]
-    U = eigenvectors
+    U = eigenvectors[:, sorted]
 
-    if not is_orthonormal(U):
-        #print("Eigenvectors are not orthonormal")
-        U = normalize_vectors(U)
-
+    eigenvalues = np.maximum(eigenvalues, 0)
     singular_values = np.sqrt(eigenvalues)
 
-    E = np.zeros((matrix.shape[0], matrix.shape[1]))
+    m, n = matrix.shape
+    E = np.zeros((m, n))
     np.fill_diagonal(E, singular_values) #np.fill_diagonal(array, values)
 
-    V = np.zeros((matrix.shape[1], matrix.shape[1]))
-    for i in range(len(singular_values)):
+    min_dim = min(m, n)
+    V = np.zeros((n, n))
+    for i in range(min_dim):
         V[:, i] = np.dot(matrix.T, U[:, i]) / singular_values[i] #ця формула дає одразу нормовані вектори
 
     return U, E, V.T
